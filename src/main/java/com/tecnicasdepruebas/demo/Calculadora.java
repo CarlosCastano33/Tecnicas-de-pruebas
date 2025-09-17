390ebbb9e90f951a80eb98bd23c34230badab2e3
Carlos Castaño <carlose.castano@udea.edu.co>
2025-09-17 01:06:20 -0500

    Calculadora todopoderosa

---
+++ src/main/java/com/tecnicasdepruebas/demo/Calculadora.java
@@ -0,0 +1,45 @@
+package com.tecnicasdepruebas.demo;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    int r = 0;
+
+    public Calculadora(){
+        r = 0;
+    }
+
+    public float operar(){
+        System.out.println("Ingrese operacion.");
+        System.out.println(" + para suma \n - para resta \n * para multiplicacion \n / para division");
+        //No se validan las entradas
+        Scanner lector = new Scanner(System.in);
+        String o = lector.nextLine();
+        if (o.equals("+")){
+            System.out.print("Ingrese operando 1: ");
+            int a = lector.nextInt();
+            System.out.print("Ingrese operando 2: ");
+            int b = lector.nextInt();
+            return (a+b);
+        } else if (o.equals("-")){
+            System.out.print("Ingrese operando 1: ");
+            int a = lector.nextInt();
+            System.out.print("Ingrese operando 2: ");
+            int b = lector.nextInt();
+            return (a-b);
+        } else if (o.equals("*")){
+            System.out.print("Ingrese operando 1: ");
+            int a = lector.nextInt();
+            System.out.print("Ingrese operando 2: ");
+            int b = lector.nextInt();
+            return (a*b);
+        } else if (o.equals("/")){
+            System.out.print("Ingrese operando 1: ");
+            int a = lector.nextInt();
+            System.out.print("Ingrese operando 2: ");
+            int b = lector.nextInt();
+            return ((float)a/b);
+        }
+        return r;
+    }
+}
--- src/main/java/com/tecnicasdepruebas/demo/DemoApplication.java
+++ src/main/java/com/tecnicasdepruebas/demo/DemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+static Calculadora calculadora = new Calculadora();
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println(calculadora.operar()*);
 	}
 
 }
